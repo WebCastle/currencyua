b4b63e97bed2c75d9e99113d34fc7c9edf85ace0
Vlad Lavro <vm.lavro@gmail.com>
2015-05-26 05:38:45 +0300

    g

--- src/Currency.php
+++ src/Currency.php
@@ -1,9 +1,22 @@
 <?php namespace Webcastle\Currency;
 
+use Illuminate\Support\Facades\Cache;
+
 class Currency{
 
-	public static function print_currency(){
+    private $currencies;
+
+	public static function fromUsd($value){
 		echo 1;
 	}
+
+    public static function usdTo($currency, $value){
+        $rate = Cache::get( 'currency_'.$currency, self::getCurrency());
+        return $rate * $value;
+    }
+
+    public function getCurrency(){
+        return 11;
+    }
 	
 }
